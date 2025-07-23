063bdf5b89ba904a48411f3d87f3d3f73097683f
anjalisharma4 <anjalisangra03@gmail.com>
2025-07-24 00:01:01 +0530

    Fix Vercel build error: commented invalid vendor import

--- vite.config.js
+++ vite.config.js
@@ -13,4 +13,11 @@ export default defineConfig({
             '$': 'jQuery'
         },
     },
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        hmr: {
+            host: 'localhost',
+        },
+    },
 });
